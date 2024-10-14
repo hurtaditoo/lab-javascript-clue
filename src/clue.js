e5b20f437a6d74043e9e1120d8effd4e30afd8a9
hurtaditoo <al415507@uji.es>
2024-10-14 13:57:22 +0200

    Iteration 2: Create the mystery

--- src/clue.js
+++ src/clue.js
@@ -101,9 +101,17 @@ const weaponsArray = [
 
 // ITERATION 2
 
-function selectRandom() {}
+function selectRandom(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
 
-function pickMystery() {}
+function pickMystery() {
+    return {
+        suspect: selectRandom(suspectsArray),
+        weapon: selectRandom(weaponsArray),
+        room: selectRandom(roomsArray)
+    };
+}
 
 
 // ITERATION 3
